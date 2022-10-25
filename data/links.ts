55efb5713bc0b2c135c3117a544279851d5cb217
Horl-Socheath <horl.socheath@gmail.com>
2022-10-25 21:27:23 +0700

    add prisma, tailwind and config db

---
+++ data/links.ts
@@ -0,0 +1,36 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+    imageUrl: "https://nextjs.org/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnotion.dbacb6a8.png&w=300&q=55",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+    imageUrl: "https://www.prisma.io/images/og-image.png",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
+    imageUrl:
+      "	https://tailwindcss.com/_next/static/media/1.da37cea3.png",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    id: "2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78",
+    imageUrl: "https://www.apollographql.com/apollo-home.jpg",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
